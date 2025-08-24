f436244a177121dc12dd5f72006fb53e97782abc
CodyBense <codybense@gmail.com>
2025-08-23 20:42:01 -0400

    following video tutorial

--- functions.py
+++ functions.py
@@ -1,54 +1,127 @@
+import oauthlib
+from requests_oauthlib import OAuth2Session
 import requests
 import pandas
 import json
 import os
 from dotenv import load_dotenv
-from urllib3 import request
+import base64
 
 
 load_dotenv()
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+oauth_client_id = os.getenv("OAUTH_CLIENT_ID")
+oauth_client_secret = os.getenv("OAUTH_CLIENT_SECRET")
+redirect_url = os.getenv("OAUTH_REDIRECT_ID")
 
 
-def get_access_token() -> str:
+def  oauth_auth():
+    oauth = OAuth2Session(oauth_client_id, redirect_uri=redirect_url)
+
+    authorization_url, state = oauth.authorization_url(
+        'https://service.com/auth',
+        access_type="offline", prompt="select_account"
+    )
+
+    print('Please visit %s and authroize acess.' % authorization_url)
+
+
+def  get_access_token():
+    auth_str = spotify_client_id + ':' + spotify_client_secret
+    auth_bytes = auth_str.encode("utf-8")
+    auth_base64  = str(base64.b64encode(auth_bytes), "utf-8")
+
     url = "https://accounts.spotify.com/api/token"
 
-    headers = {
+    headers =  {
+        "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded",
     }
 
-    payload = {
+    data = {
         "grant_type": "client_credentials",
-        "client_id": client_id,
-        "client_secret": client_secret
     }
 
-    try:
+    result =  requests.post(url, headers=headers, data=data)
 
-        response = requests.post(url, headers=headers, data=payload)
+    json_result = json.loads(result.content)
 
-        response.raise_for_status() 
+    token = json_result["access_token"]
 
-        return response.json()['access_token']
+    return token
 
-    except requests.exceptions.HTTPError as errh:
-        print(f"Http Error: {errh}")
-        return ""
-    except requests.exceptions.ConnectionError as errc:
-        print(f"Error Connecting: {errc}")
-        return ""
-    except requests.exceptions.Timeout as errt:
-        print(f"Timeout Error: {errt}")
-        return ""
-    except requests.exceptions.RequestException as err:
-        print(f"Something went wrong: {err}")
-        return ""
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+def search_for_artist(token, artist_name):
+    url =  "https://api.spotify.com/v1/search"
+    headers =  get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+
+    result = requests.get(query_url, headers=headers)
+
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exist...")
+        return None
+
+    return json_result[0]
+
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = requests.get(url, headers=headers)
+
+    json_result = json.loads(result.content)["tracks"]
+
+    return json_result
+
+
+# def get_access_token() -> str:
+#     url = "https://accounts.spotify.com/api/token"
+#
+#     headers = {
+#         "Content-Type": "application/x-www-form-urlencoded",
+#     }
+#
+#     payload = {
+#         "grant_type": "client_credentials",
+#         "client_id": spotify_client_id,
+#         "client_secret": spotify_client_secret
+#     }
+#
+#     try:
+#
+#         response = requests.post(url, headers=headers, data=payload)
+#
+#         response.raise_for_status() 
+#
+#         return response.json()['access_token']
+#
+#     except requests.exceptions.HTTPError as errh:
+#         print(f"Http Error: {errh}")
+#         return ""
+#     except requests.exceptions.ConnectionError as errc:
+#         print(f"Error Connecting: {errc}")
+#         return ""
+#     except requests.exceptions.Timeout as errt:
+#         print(f"Timeout Error: {errt}")
+#         return ""
+#     except requests.exceptions.RequestException as err:
+#         print(f"Something went wrong: {err}")
+#         return ""
 
 
 def play_pause(access_token: str):
     try:
-        url = "https://api.spotify.com/v1/me/player?market=US"
+        url = "https://api.spotify.com/v1/me/player"
 
         header = {
             "Authorization": f"Bearer {access_token}"
--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import functions
 
 
 def main():
-    access_token = functions.get_access_token()
-    print(f"is playing: {functions.play_pause(access_token)}")
+    # access_token = functions.get_access_token()
+    # print(f"{access_token}")
+    # print(f"is playing: {functions.play_pause(access_token)}")
+    # functions.oauth_auth()
+    
+    token = functions.get_access_token()
+    artist_result = functions.search_for_artist(token, "Mac Miller")
+    artist_id = artist_result["id"]
+
+    songs = functions.get_songs_by_artist(token, artist_id)
+
+    for idx, song in enumerate(songs):
+        print(f"{idx + 1}. {song['name']}")
 
 
 if __name__ == "__main__":
