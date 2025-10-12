d50da95ca2ba8295bf96d48c49e60af3f4693f8e
CodyBense <codybense@gmail.com>
2025-10-12 08:34:58 -0400

    UPDATED: README

--- help.sh
+++ help.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "To build release: cargo build --release"
-echo "To run: ./target/release/spoti <Command> <Args>"
+echo "To run: ./target/release/spoti <Command> <Args>, cargo run <Command> <Args>"
