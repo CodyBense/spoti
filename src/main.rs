e170fce134e50022c1c737ef5538dcead50698b7
CodyBense <codybense@gmail.com>
2025-10-12 08:35:25 -0400

    ADDED: outline for desired commands
    will probably expand in the future

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
-#[clap(author, version, about)]
+#[clap(
+    author = "Cody Bense",
+    version = "0.1.0",
+    about = "A silly little cli to control your spotify from the terminal instead of using the app"
+)]
 struct Args {
     #[clap(subcommand)]
     command: Command,
@@ -9,22 +13,46 @@ struct Args {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    #[clap(about = "Adds two numbers")]
-    Add {
-        #[clap(help = "The first number")]
-        a: i32,
-        #[clap(help = "The second number")]
-        b: i32,
-    }
+    // #[clap(about = "Adds two numbers")]
+    // Add {
+    //     #[clap(help = "The first number")]
+    //     a: i32,
+    //     #[clap(help = "The second number")]
+    //     b: i32,
+    // },
+    #[clap(about = "Pauses your sound")]
+    Pause {
+    },
+    #[clap(about = "Starts your sound")]
+    Start {
+    },
+    #[clap(about = "Plays a song")]
+    Play {
+    },
+    #[clap(about = "Search a song")]
+    Search {
+    },
 }
 
 fn main() {
     let args = Args::parse();
 
     match args.command {
-        Command::Add { a, b } => {
-            let result = a + b;
-            println!("Result: {}", result);
-        }
+        // Command::Add { a, b } => {
+        //     let result = a + b;
+        //     println!("Result: {}", result);
+        // }
+        Command::Pause {  } => {
+            println!("Will pause your song");
+        },
+        Command::Start {  } => {
+            println!("Will start your song");
+        },
+        Command::Play {  } => {
+            println!("Plays a song");
+        },
+        Command::Search {  } => {
+            println!("Search a song");
+        },
     }
 }
