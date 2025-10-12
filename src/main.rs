9c2830909bbf8b376b24c82a7448e33de9f0dd98
CodyBense <codybense@gmail.com>
2025-10-12 07:41:28 -0400

    TESTING

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author = "Cody Bense", version = "1.0")]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    #[command(name = "add")]
+    Add {
+        #[arg(short, long)]
+        a: i32,
+        #[arg(short, long)]
+        b: i32,
+    },
+    #[command(name = "sub")]
+    Subtract {
+        #[arg(short, long)]
+        a: i32,
+        #[arg(short, long)]
+        b: i32,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.command {
+        Command::Add { a, b } => println!("Result: {}", a + b),
+        Command::Subtract { a, b } => println!("Result: {}", a - b),
+    }
 }
